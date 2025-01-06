619e99c8344bd0e20d2ce2b47c7102871bf6a98c
Surya Tejess <suryatejessk@gmail.com>
2025-01-06 21:02:10 +0530

    active page indication for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,9 @@ const NavBar = () => {
 
   return (
     <div className="flex items-center justify-between py-5 font-medium">
-      <img src={assets.logo} className="w-36" alt="brand logo forever" />
+      <Link to="/">
+        <img src={assets.logo} className="w-36" alt="brand logo forever" />
+      </Link>
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
         <NavLink to="/" className="flex flex-col items-center gap-1 ">
           <p>HOME</p>
