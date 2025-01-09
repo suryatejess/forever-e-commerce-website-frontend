b8c5459223bb1f689ef53f685c5286869ec2dd65
Surya Tejess <suryatejessk@gmail.com>
2025-01-09 13:05:36 +0530

    Best Seller Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Hero from "../components/Hero";
 import { ShopContext } from "../context/ShopContext";
 import LatestCollection from "../components/LatestCollection";
+import BestSeller from "../components/BestSeller";
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <LatestCollection />
+      <BestSeller />
     </div>
   );
 };
