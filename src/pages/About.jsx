24f1c16c96f31c7f42c241e43b377250c05c46ac
Surya Tejess <suryatejessk@gmail.com>
2025-02-27 20:23:56 +0530

    About and Contact Pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import Title from "../components/Title";
+import { assets } from "../assets/assets";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={"ABOUT"} text2={"US"} />
+      </div>
+
+      <div className="my-10 flex flex-col md:flex-row gap-16">
+        <img
+          className="w-full md:max-w-[450px]"
+          src={assets.about_img}
+          alt="image related to us"
+        />
+        <div className="flex flex-col justify-center gap-4 md:w-2/4 texgray-600">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta
+            accusantium in modi eum suscipit fugiat, consequuntur placeat eius
+            iure sequi non temporibus similique mollitia sapiente consequatur a
+            veniam dicta nihil.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae optio
+            fugit tempore veniam nam mollitia soluta corrupti sit alias,
+            incidunt cumque saepe? Aspernatur consectetur at porro ipsum
+            expedita, repudiandae facilis.
+          </p>
+
+          <b className="text-gray-800">Our Mission</b>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Praesentium hic labore obcaecati aliquid quasi pariatur ullam velit
+            aut voluptatibus explicabo quo vero et in tempore, consectetur
+            consequatur mollitia magnam enim!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import Title from "../components/Title";
+import { assets } from "../assets/assets";
+import NewsletterBox from "../components/NewsletterBox";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <div className="text-center text-2xl pt-10 border-t">
+        <Title text1={"Contact"} text2={"Us"}></Title>
+      </div>
+
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28">
+        <img
+          className="w-full md:max-w-[480px]"
+          src={assets.contact_img}
+          alt=""
+        />
+        <div className="flex flex-col justify-center items-start gap-6">
+          <p className="font-semibold text-xl text-gray-600">Our Store</p>
+          <p className="text-gray-500">
+            52344 Alita Station <br /> Suite 377, Andhra Pradesh, India
+          </p>
+          <p className="text-gray-500">
+            Tel: (444) 4444-4432 <br /> Email: fake@forever.co
+          </p>
+          <p className="font-semibold text-xl text-gray-600">
+            Careers at Forever
+          </p>
+          <p>Learn more about our teams and job opening</p>
+          <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all">
+            Explore Jobs
+          </button>
+        </div>
+      </div>
+
+      <NewsletterBox />
+    </div>
+  );
 };
 
 export default Contact;
